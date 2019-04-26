2923a64d4ca275d1ec02054d97ede34906216d1d
Daniel Jaramillo <daniel.jaramillo7@udea.edu.co>
2019-04-26 10:01:01 -0500

    Realizando llamados POST para ingresar grabaciones

--- src/components/ReunionVideoConferencia/Grabacion.vue
+++ src/components/ReunionVideoConferencia/Grabacion.vue
@@ -5,9 +5,7 @@
       <div class="flex md6">
         <vuestic-widget :headerText="'Agregar Grabación'">
           <form>
-
             <div class="va-row">
-
               <div class="flex md8">
                 <fieldset>
                   <div class="form-group with-icon-left">
@@ -58,108 +56,7 @@
         </vuestic-widget>
       </div>
     </div>
-
-    <!-- <div class="va-row">
-      <div class="flex md12 xs12">
-        <vuestic-widget :headerText="$t('tables.styled')">
-          <div class="table-responsive">
-            <table class="table table-striped table-sm color-icon-label-table">
-              <thead>
-              <tr>
-                <td></td>
-                <td>{{'tables.headings.name' | translate}}</td>
-                <td>{{'tables.headings.email' | translate}}</td>
-                <td>{{'tables.headings.city' | translate}}</td>
-                <td align="right">{{'tables.headings.score' | translate}}</td>
-                <td align="middle"></td>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <td></td>
-                <td>Matthew McCormick</td>
-                <td>matthew30@mail.ol</td>
-                <td>Vancouver</td>
-                <td align="right">93</td>
-                <td align="middle"></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>Nancy Bo</td>
-                <td>nancy@boonweb.com</td>
-                <td>Washington</td>
-                <td align="right">280</td>
-                <td align="middle"></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>Frederiko Lopez</td>
-                <td>fr.lopez@webmail.sp</td>
-                <td>Barcelona</td>
-                <td align="right">16</td>
-                <td align="middle"></td>
-              </tr>
-              <tr class="table-danger">
-                <td>
-                  <span class="badge badge-pill badge-danger">DANGER</span>
-                </td>
-                <td>Stanley Hummer</td>
-                <td>mr_winner_2999@gmail.cb</td>
-                <td>Manchester</td>
-                <td align="right">57</td>
-                <td align="middle">
-                  <i
-                    class="fa fa-exclamation-triangle icon-right input-icon error-icon"></i>
-                </td>
-              </tr>
-              <tr class="table-success">
-                <td>
-                  <span class="badge badge-pill badge-primary">SUCCESS</span>
-                </td>
-                <td>Lendley Wintz</td>
-                <td>9938198146@mailster.io</td>
-                <td>Wien</td>
-                <td align="right">113</td>
-                <td align="middle" class="valid">
-                  <i class="fa fa-check success-icon icon-right input-icon"></i>
-                </td>
-              </tr>
-              <tr class="table-warning">
-                <td>
-                  <span class="badge badge-pill badge-warning">WARNING</span>
-                </td>
-                <td>Barbara Noz</td>
-                <td>barbaranoz@mailster.io</td>
-                <td>Brussels</td>
-                <td align="right">68</td>
-                <td align="middle"></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>Matthew McCormick</td>
-                <td>matthew30@mail.ol</td>
-                <td>Vancouver</td>
-                <td align="right">93</td>
-                <td align="middle"></td>
-              </tr>
-              <tr class="table-info">
-                <td>
-                  <span class="badge badge-pill badge-info">INFO</span>
-                </td>
-                <td>Nancy Bo</td>
-                <td>nancy@boonweb.com</td>
-                <td>Washington</td>
-                <td align="right">280</td>
-                <td align="middle"></td>
-              </tr>
-              </tbody>
-            </table>
-          </div>
-        </vuestic-widget>
-      </div>
-    </div> -->
-
-    </div>
+  </div>
 </template>
 
 <script>
@@ -195,7 +92,17 @@ export default {
   },
   methods: {
     mostrarGrabaciones () {
-      console.log(this.grabaciones[0])
+      console.log(this.grabaciones[0]);
+      axios.post('http://localhost:3000/grabaciones', {
+        "id": 8,
+        "idexterno": "idexterno8",
+        "ocurrenciaId": 3
+      }).then(res => {
+        console.log(res);
+        this.grabaciones.push(res.data);
+      }).catch(error => {
+        console.log(error);
+      })
     }
   },
   beforeCreate () {
